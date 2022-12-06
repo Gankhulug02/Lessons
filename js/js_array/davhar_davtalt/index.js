7fcb878060362f46103b663fb37de4954846de2c
Gankhulug <huluguu0202@gmail.com>
2022-12-06 22:08:33 +0800

    12.6 at home

--- js/js_array/davhar_davtalt/index.js
+++ js/js_array/davhar_davtalt/index.js
@@ -1,9 +1,9 @@
 // input = 5;
-output ="";
+// output ="";
 
-for(let input = 0; input > 5; input=++ ){
-    console.log(input)
-    for(let i=1; i<input; i++){
+// for(let input = 0; input > 5; input=++ ){
+//     console.log(input)
+//     for(let i=1; i<input; i++){
         
-    }
-}
+//     }
+// }
--- js/js_array/js_function/index.js
+++ js/js_array/js_function/index.js
@@ -18,12 +18,15 @@ let max = (a, b,c) =>{
 console.log (max(7,5,10));
 
 
-//2. Өгөгдсөн array -аас өгөгдсөн тоо байгаа эсэхийг олох функц бич. Тухайн тоо байвал индексийг буцаана. Байхгүй бол -1 буцаана.
+
+
+
 console.log("2. Өгөгдсөн array -аас өгөгдсөн тоо байгаа эсэхийг олох функц бич. Тухайн тоо байвал индексийг буцаана. Байхгүй бол -1 буцаана.")
 
 let b = [1,2,3,4,5,6,7,8,9]
 let c = 9;
 let hariu ;
+console.log(b);
 let findNumber = (arrNumber,c) =>{
     // let i = 0;
     for (let i=0; i < arrNumber.length; i++){
@@ -39,11 +42,15 @@ let findNumber = (arrNumber,c) =>{
 
 console.log("tanii haisan too", + c + ": " +findNumber(b,c)+ "d baina");
 
-// Өгөгдсөн тэмдэгт мөрөөс -аас өгөгдсөн тэмдэгт байгаа эсэхийг олох функц бич. Тухайн тэмдэгт байвал индексийг буцаана. Байхгүй бол -1 буцаана.
+
+
+
+console.log("3. Өгөгдсөн тэмдэгт мөрөөс -аас өгөгдсөн тэмдэгт байгаа эсэхийг олох функц бич. Тухайн тэмдэгт байвал индексийг буцаана. Байхгүй бол -1 буцаана.");
 
 let d = ['a','b','c','d','e','f','g'];
 let e = 'd';
 let hariuText =""; 
+console.log(d)
 
 let findText = (arrText, e) => {
     let i=0;
@@ -57,4 +64,55 @@ let findText = (arrText, e) => {
     }
     return hariuText
 }
-console.log(findText(d,e));
+console.log("tanii haisan ug", e +": "+ findText(d,e) + "t bn");
+
+
+
+
+
+console.log("5. Өгөгдсөн array - ийн дундажыг олох функц бич.")
+
+d = [12,45,23,48,15,23];
+let sum = 0;
+console.log('array = '+d)
+
+let findAverage = (arrAverage) => {
+    let i = 0;
+    for (let i=0; i<arrAverage.length; i++){
+        sum = sum + arrAverage[i] 
+    }
+    sum = sum / arrAverage.length
+    return sum
+}
+console.log(findAverage(d))
+
+
+
+
+
+console.log('6. Өгөгдсөн array - ийн нийлбэрийг олох функц бич.')
+
+console.log('array = '+d)
+sum = 0;
+
+let arrAdd = (arrAdd) => {
+    let i = 0;
+    for (let i=0; i<arrAdd.length; i++){
+        sum = sum + arrAdd[i] 
+    }
+    // sum = sum / arrAdd.length
+    return sum
+}
+console.log(arrAdd(d))
+
+
+
+
+
+console.log("8. Өгөгдсөн тэмдэгт мөрийн бүх үсгийг том үсэг болгох функц бич.")
+
+d = ['mongol','china','russia','german','sweden','turkey',]
+
+let arrUpper = (arrUpper) => {
+    
+}
