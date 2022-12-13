9730d296d63e55f0733a9df048539ebdb1a788d1
Gankhulug <huluguu0202@gmail.com>
2022-12-13 11:52:49 +0800

    12.13

--- js/js_DOM/index.js
+++ js/js_DOM/index.js
@@ -6,40 +6,57 @@
 // document.querySelectorAll(".profile")[0].style.color = "Azure";
 // document.querySelectorAll(".profile")[0].style.fontSize = "222px";
 
-const color = ["red", "green","blue"]
-let colorindex= 0;
+// const color = ["red", "green","blue"]
+// let colorindex= 0;
 
-// const box = document.getElementsByTagName("box")[1];
-const divs = document.getElementsByClassName("box1");
-const change = ( ) => {
-    if(colorindex >= 3){
-        colorindex = 0;
-    }
-    let r = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let g = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let b = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let r1 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let g1 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let b1 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let r2 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let g2 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let b2 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let r3 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let g3 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let b3 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let r4 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let g4 = Math.floor(Math.random() * (255 - 0)).toString(16)
-    let b4 = Math.floor(Math.random() * (255 - 0)).toString(16)
+// // const box = document.getElementsByTagName("box")[1];
+// const divs = document.getElementsByClassName("box1");
+// const change = ( ) => {
+//     if(colorindex >= 3){
+//         colorindex = 0;
+//     }
+//     let r = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let g = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let b = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let r1 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let g1 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let b1 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let r2 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let g2 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let b2 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let r3 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let g3 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let b3 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let r4 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let g4 = Math.floor(Math.random() * (255 - 0)).toString(16)
+//     let b4 = Math.floor(Math.random() * (255 - 0)).toString(16)
 
-    box1.style.backgroundColor = `#${r}${g}${b}`
-    box2.style.backgroundColor = `#${r1}${g1}${b1}`
-    box3.style.backgroundColor = `#${r2}${g2}${b2}`
-    box4.style.backgroundColor = `#${r3}${g2}${b3}`
-    box5.style.backgroundColor = `#${r4}${g2}${b4}`
-    // body.style = `background-color: ${color[colorindex]}`;
-    colorindex ++;
-}
+//     box1.style.backgroundColor = `#${r}${g}${b}`
+//     box2.style.backgroundColor = `#${r1}${g1}${b1}`
+//     box3.style.backgroundColor = `#${r2}${g2}${b2}`
+//     box4.style.backgroundColor = `#${r3}${g2}${b3}`
+//     box5.style.backgroundColor = `#${r4}${g2}${b4}`
+//     // body.style = `background-color: ${color[colorindex]}`;
+//     colorindex ++;
+// }
 
-myBtn.addEventListener("click", change)
-myBtn1.addEventListener("click", change)
-myBtn2.addEventListener("click", change)
+// myBtn.addEventListener("click", change)
+// myBtn1.addEventListener("click", change)
+// myBtn2.addEventListener("click", change)
+
+      const myBtn = document.getElementsByClassName("btn")[0];
+      const body = document.getElementsByTagName("body")[0];
+      const divs = document.getElementsByClassName("box");
+      const change = () => {
+        for (let i = 0; i < divs.length; i++) {
+          let color = getColor();
+          divs[i].style.backgroundColor = color;
+        }
+      };
+      const getColor = () => {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        return `rgb(${red}, ${green}, ${blue})`;
+      };
+      myBtn.addEventListener("click", change);
