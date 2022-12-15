a759d4fd1571bca2b251f5195e143772e4e40589
Gankhulug <huluguu0202@gmail.com>
2022-12-15 13:42:10 +0800

    12.15

--- js/js_BOM/To_do_list/index.js
+++ js/js_BOM/To_do_list/index.js
@@ -2,14 +2,32 @@ const addBtn = document.getElementById("add");
 const taskList = document.getElementById("tasks");
 const inputText = document.getElementById("to-do-input");
 
+const ustgah = (e) => {
+    // alert("Ustgagdlaa.");
+    const parent = e.parentNode.parentNode.parentNode;
+    const child = e.parentNode.parentNode;
+    parent.removeChild(child)
+}
+
+const check = (e)=>{
+    let a=e.parentNode.parentNode;
+    a.classList.add("check");
+    // console.log(a)
+}
+const edit = (e)=>{
+    let a=e.parentNode.parentNode.parentNode;
+    // a.removeAttribute("readonly");
+    console.log(a)
+    // console.log(a.firstChild)
+}
 const cardItem = (text) =>{ 
-    const item = `
+    const item = `   
                 <div class="task">
-                    <input type="text" class="text" readonly value="${text}" />
+                    <input type="text" id="text" class="text" readonly value="${text}" />
                     <div class="actions">
-                        <button class="edit"><i class="fas fa-pen"></i></button>
-                        <button class="done"><i class="fas fa-check"></i></button>
-                        <button class="delete"><i class="fas fa-trash"></i></button>
+                        <button class="edit" onclick="edit(this)"><i class="fas fa-pen"></i></button>
+                        <button class="done" onclick="check(this)"><i class="fas fa-check"></i></button>
+                        <button class="delete" onclick="ustgah(this)"><i class="fas fa-trash"></i></button>
                     </div>
                 </div>`;
     return item;
@@ -17,13 +35,13 @@ const cardItem = (text) =>{
 
 const taskAdd = () => {
     let text = inputText.value;
-    inputText.value = ""
     if(text === ""){
         alert("Ta utga oruulaagui baina");
     }else{
-
+        
         taskList.innerHTML += cardItem(text);
     }
+    inputText.value = "";
 }
 
 addBtn.addEventListener("click",taskAdd);
