2c68d326deb3703e928d36db45ba446454a60b35
Gankhulug <huluguu0202@gmail.com>
2022-12-22 20:26:40 +0800

    12.22

--- js/Calculator/java.js
+++ js/Calculator/java.js
@@ -1,30 +1,31 @@
-let numbersBtn = document.getElementsByClassName("number");
-let aBtn = document.querySelectorAll("[data-value");
+// let numbersBtn = document.getElementsByClassName("number");
+let numbersBtn = document.querySelectorAll("[data-value");
+let operationBtn = document.querySelectorAll("[data-operation");
 let display = document.getElementById("display")
-let plusBtn = document.getElementsByClassName("operator")[3];
-let equalsBtn = document.getElementsByClassName("operator")[4];
-let minusBtn = document.getElementsByClassName("operator")[2];
-let timesBtn = document.getElementsByClassName("operator")[1];
-let orBtn = document.getElementsByClassName("operator")[0];
+// let plusBtn = document.getElementsByClassName("operator")[3];
+// let equalsBtn = document.getElementsByClassName("operator")[4];
+// let minusBtn = document.getElementsByClassName("operator")[2];
+// let timesBtn = document.getElementsByClassName("operator")[1];
+// let orBtn = document.getElementsByClassName("operator")[0];
 
 // console.log(minusBtn.outerHTML)
-console.log(aBtn)
+console.log(operationBtn)
 
-// for(let i=0; i<numbersBtn.length; i++){
-//     numbersBtn[i].addEventListener("click", ()=>{
-//     display.textContent += numbersBtn[i].textContent;
-//     console.log(display.textContent)
-//   })
-// }
-for(let i=0; i<aBtn.length; i++){
-    aBtn[i].addEventListener("click", ()=>{
-    display.textContent += aBtn[i].textContent;
+for(let i=0; i<numbersBtn.length; i++){
+    numbersBtn[i].addEventListener("click", ()=>{
+    display.textContent += numbersBtn[i].textContent;
     console.log(display.textContent)
   })
 }
+// for(let i=0; i<aBtn.length; i++){
+//     aBtn[i].addEventListener("click", ()=>{
+//     display.textContent += aBtn[i].textContent;
+//     console.log(display.textContent)
+//   })
+// }
 
 let plus = () => {
 
 } 
-plusBtn.addEventListener("click", plus); 
+// plusBtn.addEventListener("click", plus); 
 // equalsBtn.addEventListener("click", equals); 
