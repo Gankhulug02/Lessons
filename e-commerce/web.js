2cfda4bbeb86c61a4a9ceb6ce300bd9ef6ffc0ee
Gankhulug <huluguu0202@gmail.com>
2022-12-26 23:51:54 +0800

    12.26

--- e-commerce/web.js
+++ e-commerce/web.js
@@ -1,6 +1,8 @@
 console.log("ECOMMERCE")
+
 //all vaiables
 const productsList = document.querySelector(".items");
+const items = document.querySelector(".sidebar-items")
 let sagsButton = document.querySelector(".sagsBtn")
 let sidebar = document.querySelector(".sidebar")
 let allProducts = [] ;
@@ -38,7 +40,7 @@ const displayProduct = () => {
                                 <i class="fa fa-star"></i>
                                 <p>${product.rating}</p>
                             </div>
-                            <button class="text-end">Нэмэх</button>
+                            <button class="text-end "onclick="nemeh(this)">Нэмэх</button>
                         </div>
                         </div>
                     </div>
@@ -52,3 +54,51 @@ sags = () => {
     sidebar.classList.toggle("sags2")
 }
 sagsButton.addEventListener("click", sags)
+
+//Sidebar iin innerHTML nemeh function
+const itemsnemeh = () => {
+    let item = `<div class="sidebar-item d-flex flex-col justify-content-around">
+            <div class="count d-flex flex-column justify-content-around">
+                <button onclick="countNemeh(this)">+</button>
+                <p class="m-0 text-center">1</p>
+                <button onclick="countHasah(this)">-</button>
+            </div>
+            <img src="../image/e-commerce/iphone9 1ip9.svg" alt="" class="w-25">
+            <div class="sidebar-item-title w-25">
+                <p class="text-truncate">iPhone 9</p>
+                <p class="m-0">500</p>
+                <p class="m-0 text-secondary">1</p>
+            </div>
+            <p class="my-auto h-25">$12345</p>
+            <button class="border-0 bg-white text-secondary"><i class="fa fa-x"></i></button>
+        </div>`
+    items.innerHTML += item
+}
+
+const nemeh = (e) => {
+    console.log("asd")
+    itemsnemeh()
+}
+//Sidebar iin count hiih buttonuud
+
+const countNemeh = (e) => {
+    a = e.parentNode.children[1];
+    b = e.parentNode.parentNode.children[2].children;
+    c = b[1];
+    d = e.parentNode.parentNode.children[3]
+
+    a.innerHTML ++ ;
+    b[2].innerHTML = a.innerHTML
+    d.innerHTML = "$" + b[1].innerHTML * b[2].innerHTML
+}
+const countHasah = (e) => {
+    a = e.parentNode.children[1];
+    b = e.parentNode.parentNode.children[2].children;
+    c = b[1];
+    d = e.parentNode.parentNode.children[3]
+
+    a.innerHTML -- ;
+    b[2].innerHTML = a.innerHTML
+    d.innerHTML = "$" + b[1].innerHTML * b[2].innerHTML
+}
+
